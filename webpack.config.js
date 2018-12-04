1f1b49b1f63a4e1f3c2be216f704813d7ecf4f87
lovewcc <148373644@qq.com>
2018-12-04 15:43:22 +0800

    修改webpack配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,120 +2,123 @@
  * @Author: Jerrychan
  * @Date: 2018-11-08 13:38:04
  * @LastEditors: Jerrychan
- * @LastEditTime: 2018-11-08 13:38:27
+ * @LastEditTime: 2018-12-04 15:42:43
  * @Description: webpack配置文件
  */
 
-const path              = require('path');
-const webpack           = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+/**
+ *  @process.env webpack环境配置中传入的变量
+ *  @dev 开发环境
+ */
 let WEBPACK_ENV = process.env.WEBPACK_ENV || 'dev';
-console.log(WEBPACK_ENV); 
+console.log(WEBPACK_ENV == 'dev' ? '开发环境' : '线上环境');
 module.exports = {
-    entry: './src/app.jsx',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: WEBPACK_ENV === 'dev' 
-            ? '/dist/' : '//admin.52react.cn/dist/',
-        filename: 'js/app.js'
-    },
-    resolve: {
-        alias : {
-            node_modules        : path.resolve(__dirname, 'node_modules'),
-            page        : path.resolve(__dirname, 'src/page'),
-            component   : path.resolve(__dirname, 'src/component'),
-            util        : path.resolve(__dirname, 'src/util'),
-            service     : path.resolve(__dirname, 'src/service')
-        }
-    },
-    module: {
-        rules: [
-            // react(jsx)语法的处理
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env', 'react']
-                    }
-                }
-            },
-            // css文件的处理
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: "css-loader"
-                })
-            },
-            // sass文件的处理
-            {
-                test: /\.less$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'less-loader']
-                })
-            },
-            // 图片的配置
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            name: 'resource/[name].[ext]'
-                        }
-                    }
-                ]
-            },
-            // 字体图标的配置
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192,
-                            name: 'resource/[name].[ext]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        // 处理html文件 
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            favicon: './favicon.ico'
-        }),
-        // 独立css文件
-        new ExtractTextPlugin("css/[name].css"),
-        // 提出公共模块
-        new webpack.optimize.CommonsChunkPlugin({
-            name : 'common',
-            filename: 'js/base.js'
-        })
-    ],
-    devServer: {
-        port: 8086,
-        historyApiFallback: {
-            index: '/dist/index.html'
-        },
-        open:true,
-        proxy : {
-            '/manage' : {
-                target: 'http://admintest.happymmall.com',
-                changeOrigin : true
-            },
-            '/user/logout.do' : {
-                target: 'http://admintest.happymmall.com',
-                changeOrigin : true
-            }
-        }
-    }
+	entry: './src/app.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: WEBPACK_ENV === 'dev' ? '/dist/' : '//admin.52react.cn/mall_admin/dist/',
+		filename: 'js/app.js',
+	},
+	resolve: {
+		alias: {
+			node_modules: path.resolve(__dirname, 'node_modules'),
+			page: path.resolve(__dirname, 'src/page'),
+			component: path.resolve(__dirname, 'src/component'),
+			util: path.resolve(__dirname, 'src/util'),
+			service: path.resolve(__dirname, 'src/service'),
+		},
+	},
+	module: {
+		rules: [
+			// react(jsx)语法的处理
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['env', 'react'],
+					},
+				},
+			},
+			// css文件的处理
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader',
+				}),
+			},
+			// sass文件的处理
+			{
+				test: /\.less$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'less-loader'],
+				}),
+			},
+			// 图片的配置
+			{
+				test: /\.(png|jpg|gif)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name: 'resource/[name].[ext]',
+						},
+					},
+				],
+			},
+			// 字体图标的配置
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name: 'resource/[name].[ext]',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		// 处理html文件
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			favicon: './favicon.ico',
+		}),
+		// 独立css文件
+		new ExtractTextPlugin('css/[name].css'),
+		// 提出公共模块
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'common',
+			filename: 'js/base.js',
+		}),
+	],
+	devServer: {
+		port: 8086,
+		historyApiFallback: {
+			index: '/dist/index.html',
+		},
+		open: true,
+		proxy: {
+			'/manage': {
+				target: 'http://admintest.happymmall.com',
+				changeOrigin: true,
+			},
+			'/user/logout.do': {
+				target: 'http://admintest.happymmall.com',
+				changeOrigin: true,
+			},
+		},
+	},
 };
